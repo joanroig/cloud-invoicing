e2c947c6118dcab0d6522bbe09f0d67c5e82527d
Joan Roig <12451419+joanroig@users.noreply.github.com>
2022-03-11 00:17:48 +0100

    feat: start the csv process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello world!");
+console.log("> Rechnungsprogramm Start");
+console.log("-------------------------");
+
+import * as csv from "fast-csv";
+import * as fs from "fs";
+import * as path from "path";
+
+type RechnungRow = {
+  date: string;
+  name: string;
+  fromEmailAddress: string;
+};
+
+function camelCase(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+}
+
+fs.createReadStream(path.resolve(__dirname, "..", "assets", "parse.csv"))
+  .pipe(
+    csv.parse({
+      headers: (headerArray) => headerArray.map((header) => camelCase(header!)),
+    })
+  )
+  .on("error", (error) => console.error(error))
+  .on("data", (row: RechnungRow) => {
+    // console.log(row);
+    console.log(row.date);
+    console.log(row.name);
+    console.log(row.fromEmailAddress);
+    console.log("---------------");
+  })
+  .on("end", (rowCount: number) => {
+    console.log(`Parsed ${rowCount} rows`);
+  });
